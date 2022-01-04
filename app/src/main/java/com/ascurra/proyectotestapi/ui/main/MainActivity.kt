bd7620b069c93ef5fbf181e65595eaaf9f514b47
Gabriel Ascurra <gabiiascurra@gmail.com>
2022-01-04 17:27:50 -0300

    Se arreglo un problema al volver hacia atras en la pantalla de inicio

--- app/src/main/java/com/ascurra/proyectotestapi/ui/main/MainActivity.kt
+++ app/src/main/java/com/ascurra/proyectotestapi/ui/main/MainActivity.kt
@@ -11,6 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction().add(R.id.contenedor,PokemonListFragment()).addToBackStack("Busqueda").commit()
+        supportFragmentManager.beginTransaction().add(R.id.contenedor,PokemonListFragment()).commit()
     }
 }
